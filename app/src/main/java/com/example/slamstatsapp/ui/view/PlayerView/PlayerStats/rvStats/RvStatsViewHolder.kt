fd878432eaa6165ffaab035e1900a3243213eb8c
Mariskii <vicentpayaginer@gmail.com>
2024-03-10 13:32:16 +0100

    Added fading effect in the RecyclerViews and the horizontal scroll view

--- app/src/main/java/com/example/slamstatsapp/ui/view/PlayerView/PlayerStats/rvStats/RvStatsViewHolder.kt
+++ app/src/main/java/com/example/slamstatsapp/ui/view/PlayerView/PlayerStats/rvStats/RvStatsViewHolder.kt
@@ -16,11 +16,12 @@ class RvStatsViewHolder(view: View):RecyclerView.ViewHolder(view)
         binding.playedGames.text = stats.pj.toString()
         binding.assistsPerGame.text = stats.app.toString()
         binding.blocksPerGame.text = stats.blk.toString()
+        binding.stealsPerGame.text = stats.stl.toString()
         binding.fieldGoalPerc.text = stats.tc
-        binding.freeThrowGoalPerc.text = stats.tl
+        binding.freethrowPerc.text = stats.tl
         binding.pointsPerGame.text = stats.ppp.toString()
         binding.reboundsPerGame.text = stats.rpp.toString()
         binding.threePointsMade.text = stats.pm3.toString()
-        binding.perc3.text = stats.perc3
+        binding.threePointsPerc.text = stats.perc3
     }
 }
